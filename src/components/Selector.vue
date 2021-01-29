e53f9e93703fd3696f0d8d18a78726d18b28cc0c
Byron Hall <byron.hall@ricardodefense.com>
2021-01-29 13:55:07 -0800

    integrated selector component with vuex

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -1,13 +1,49 @@
 <template>
-  <div></div>
+  <div>
+    <div
+      v-for="(service, index) in recognizedServiceBranches"
+      :key="index + service"
+    >
+      <input
+        v-model="checkedServices"
+        type="checkbox"
+        :value="service"
+        @click="toggleServiceBranch(service)"
+      />
+      <label style="cursor: pointer;" @click="toggleServiceBranch(service)">{{service}}</label>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
     name: 'Selector',
-    mounted(){
-        console.log(this.$store.state.recognizedServiceBranches);
-    }
+    computed: {
+      recognizedServiceBranches(){
+        return this.$store.state.recognizedServiceBranches;
+      },
+      checkedServices: {
+        get: function(){
+          return this.$store.state.checkedServices;
+        },
+        set: function(){},
+      },
+    },
+    methods: {
+      toggleServiceBranch(service){
+        const tempServiceBranchArray = [];
+        for(let i = 0; i < this.checkedServices.length; i+=1){
+          tempServiceBranchArray.push(this.checkedServices[i]);
+        }
+        const index = tempServiceBranchArray.indexOf(service);
+        if(index > -1){
+          tempServiceBranchArray.splice(index, 1);
+        } else {
+          tempServiceBranchArray.push(service);
+        }
+        this.$store.state.checkedServices = tempServiceBranchArray;
+      },
+    },
 }
 </script>
 

ea0a3483a6ac6290043dbf478eba2b534155c836
Byron Hall <byron.hall@ricardodefense.com>
2021-01-29 13:09:42 -0800

    added selector component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Selector />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Selector from '@/components/Selector';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Selector,
   }
 }
 </script>
---
+++ src/components/Selector.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+    name: 'Selector',
+    mounted(){
+        console.log(this.$store.state.recognizedServiceBranches);
+    }
+}
+</script>
+
+<style>
+
+</style>

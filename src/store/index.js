b3bba5433951bcff50dc353c9007bc331098ea88
Byron Hall <byronhall@MacBook-Pro.local>
2021-05-31 10:11:09 -0500

    added get nodes at depth function
    functioning setting of depth. TODO fix CSS again

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,8 @@ export default new Vuex.Store({
   state: {
     recognizedServiceBranches,
     checkedServices: recognizedServiceBranches,
+    displayDepth: 2,
+    maxTreeDepth: null,
   },
   mutations: {
   },
